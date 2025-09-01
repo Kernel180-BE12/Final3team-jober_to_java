45c42e961ec4aaafbc3ce3b143cc6407bfc8b274
mseo39 <a01063294905@gmail.com>
2025-09-01 21:25:58 +0900

    feat: 페이지네이션 공통 응답

---
+++ src/main/java/com/example/final_projects/dto/PageResponse.java
@@ -0,0 +1,15 @@
+package com.example.final_projects.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PageResponse<T> {
+    private final List<T> data;
+    private final int page;
+    private final int size;
+    private final long total;
+}
