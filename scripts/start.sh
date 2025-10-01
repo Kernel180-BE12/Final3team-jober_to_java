556c50d2b1c4677ef1a7ab1640dc0fbe98470924
mseo39 <a01063294905@gmail.com>
2025-10-01 20:17:34 +0900

    fix: Dspring.application.name 옵션 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ JAR_FILE_PATH=$(find /home/ubuntu/target -name "*.jar" | head -n 1)
 LOG_FILE=/home/ubuntu/$APP_NAME.log
 
 echo "Starting $APP_NAME with prod profile..."
-nohup java -jar $JAR_FILE_PATH --spring.profiles.active=prod > $LOG_FILE 2>&1 &
+nohup java -Dspring.application.name=$APP_NAME -jar $JAR_FILE_PATH --spring.profiles.active=prod > $LOG_FILE 2>&1 &
